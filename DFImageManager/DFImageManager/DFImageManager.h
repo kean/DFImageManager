460b5221aa08b46f50c290c0f865cd1fa28fe114
kean <grebenyuk.alexander@gmail.com>
2014-10-31 00:58:16 -1000

    initial commit

---
+++ DFImageManager/DFImageManager/DFImageManager.h
@@ -0,0 +1,13 @@
+//
+//  DFImageManager.h
+//  DFImageManager
+//
+//  Created by Alexander Grebenyuk on 10/31/14.
+//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DFImageManager : NSObject
+
+@end
---
+++ DFImageManager/DFImageManager/DFImageManager.m
@@ -0,0 +1,13 @@
+//
+//  DFImageManager.m
+//  DFImageManager
+//
+//  Created by Alexander Grebenyuk on 10/31/14.
+//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//
+
+#import "DFImageManager.h"
+
+@implementation DFImageManager
+
+@end
