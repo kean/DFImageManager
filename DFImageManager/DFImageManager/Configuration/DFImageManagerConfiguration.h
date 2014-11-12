0b6950ebbac8856e2d9b643053af0d54e2f0c598
kean <grebenyuk.alexander@gmail.com>
2014-11-12 01:35:17 -1000

    Add default DFImageManagerConfiguration (not complete implementation)

---
+++ DFImageManager/DFImageManager/Configuration/DFImageManagerConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  DFImageManagerConfiguration.h
+//  DFImageManager
+//
+//  Created by Alexander Grebenyuk on 11/11/14.
+//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DFImageManagerConfiguration : NSObject
+
+@end
---
+++ DFImageManager/DFImageManager/Configuration/DFImageManagerConfiguration.m
@@ -0,0 +1,13 @@
+//
+//  DFImageManagerConfiguration.m
+//  DFImageManager
+//
+//  Created by Alexander Grebenyuk on 11/11/14.
+//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//
+
+#import "DFImageManagerConfiguration.h"
+
+@implementation DFImageManagerConfiguration
+
+@end
