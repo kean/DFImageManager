08bbf9628060e60f4d21c620c82fc81428c00aa8
kean <grebenyuk.alexander@gmail.com>
2014-11-12 18:34:45 -1000

    Sample works

---
+++ DFImageManager/DFImageManager/Operations/DFImageFetchConnectionOperation.h
@@ -0,0 +1,13 @@
+//
+//  DFImageFetchConnectionOperation.h
+//  DFImageManager
+//
+//  Created by Alexander Grebenyuk on 11/12/14.
+//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//
+
+#import "DFURLConnectionOperation.h"
+
+@interface DFImageFetchConnectionOperation : DFURLConnectionOperation
+
+@end
---
+++ DFImageManager/DFImageManager/Operations/DFImageFetchConnectionOperation.m
@@ -0,0 +1,13 @@
+//
+//  DFImageFetchConnectionOperation.m
+//  DFImageManager
+//
+//  Created by Alexander Grebenyuk on 11/12/14.
+//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//
+
+#import "DFImageFetchConnectionOperation.h"
+
+@implementation DFImageFetchConnectionOperation
+
+@end
