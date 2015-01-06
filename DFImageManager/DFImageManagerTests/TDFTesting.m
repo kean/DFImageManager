56c865320520e64a12386f3af71b24eb935435e5
Alexander Grebenyuk <grebenyuk.alexander@gmail.com>
2015-01-05 21:09:57 -1000

    Update copyright date

--- DFImageManager/DFImageManagerTests/TDFTesting.m
+++ DFImageManager/DFImageManagerTests/TDFTesting.m
@@ -3,7 +3,7 @@
 //  DFImageManager
 //
 //  Created by Alexander Grebenyuk on 12/26/14.
-//  Copyright (c) 2014 Alexander Grebenyuk. All rights reserved.
+//  Copyright (c) 2015 Alexander Grebenyuk. All rights reserved.
 //
 
 #import "TDFTesting.h"
