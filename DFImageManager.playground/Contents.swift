20bf38e9e2d58c06444e4e1a052e6c3cd83429d2
Alexander Grebenyuk <grebenyuk.alexander@gmail.com>
2015-06-28 20:03:52 +0300

    Improved playground

--- DFImageManager.playground/Contents.swift
+++ DFImageManager.playground/Contents.swift
@@ -3,20 +3,28 @@
 import UIKit
 import DFImageManagerKit
 import XCPlayground
+//: ## DFImageManager
 
 let manager = DFImageManager.sharedManager()
 
+//: Zero config image fetching. Use shared manager to request an image for the given URL. The completion block is called with a decompressed, fullsize image.
 let imageURL = NSURL(string: "http://farm8.staticflickr.com/7315/16455839655_7d6deb1ebf_z_d.jpg")!
-
-// Request fullsize image
-manager.requestImageForResource(imageURL) { (image: UIImage?, [NSObject : AnyObject]) -> Void in
+manager.requestImageForResource(imageURL) { (image: UIImage?, _) -> Void in
     var fetchedImage = image
 }
 
-// Request scaled image
+//: Use DFImageRequest class to set specific request parameters like output image size (in pixels).
 let request = DFImageRequest(resource: imageURL, targetSize: CGSize(width: 100, height: 100), contentMode: .AspectFill, options: nil)
-manager.requestImageForRequest(request) { (image: UIImage?, [NSObject : AnyObject]) -> Void in
+manager.requestImageForRequest(request) { (image: UIImage?, _) -> Void in
     var fetchedImage = image
 }
+
+//: Image manager returns instance of DFImageRequestID class for each image request. Request ID can be used to cancel request or change its priority.
+let requestID = manager.requestImageForResource(NSURL(string: "http://farm6.staticflickr.com/5311/14244377986_c3c660ef30_k_d.jpg")!, completion: { (image, info) -> Void in
+    var fetchedImage = image
+    let error = info[DFImageInfoErrorKey] as! NSError
+})
+requestID.setPriority(.High)
+requestID.cancel()
 
 XCPSetExecutionShouldContinueIndefinitely()
