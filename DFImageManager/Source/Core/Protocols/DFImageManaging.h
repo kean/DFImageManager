10f646e09774e4a6b438443f548e95e7a69c4128
kean <grebenyuk.alexander@gmail.com>
2015-07-11 14:39:30 +0300

    DF25: Mark requestImageForResource:, requestImageForRequest: methods as deprecated

--- DFImageManager/Source/Core/Protocols/DFImageManaging.h
+++ DFImageManager/Source/Core/Protocols/DFImageManaging.h
@@ -39,17 +39,6 @@ typedef void (^DFImageRequestCompletion)(UIImage *__nullable image, NSDictionary
  */
 - (BOOL)canHandleRequest:(DFImageRequest *)request;
 
-/*! Requests an image representation with a maximum available size for the specified resource.
- */
-- (nullable DFImageTask *)requestImageForResource:(id)resource completion:(void (^__nullable)(UIImage *__nullable image, NSDictionary *info))completion;
-
-/*! Requests an image representation for the specified request.
- @param request The request that contains the resource whose image it to be loaded as well as other request options. The implementation should create a deep copy of the request so that it can't be changed underneath it later. The implementation may provide more request options that are available in a base class, so make sure to check the documentation on that.
- @param completion Can be called synchronously. A block to be called when image loading is complete, providing the requested image or information about the status of the request. The info dictionary provides information about the status of the request. See the definitions of DFImageInfo*Key strings for possible keys and values.
- @return An image task.
- */
-- (nullable DFImageTask *)requestImageForRequest:(DFImageRequest *)request completion:(void (^__nullable)(UIImage *__nullable image, NSDictionary *info))completion;
-
 /*! Cancels all outstanding requests, including preheating requests, and then invalidates the image manager. New requests may not be created.
  */
 - (void)invalidateAndCancel;
@@ -66,6 +55,19 @@ typedef void (^DFImageRequestCompletion)(UIImage *__nullable image, NSDictionary
 /*! Cancels all image preheating requests registered with a manager.
  */
 - (void)stopPreheatingImagesForAllRequests;
+
+#pragma mark - Deprecated
+
+/*! Requests an image representation with a maximum available size for the specified resource.
+ */
+- (nullable DFImageTask *)requestImageForResource:(id)resource completion:(void (^__nullable)(UIImage *__nullable image, NSDictionary *info))completion DEPRECATED_ATTRIBUTE;
+
+/*! Requests an image representation for the specified request.
+ @param request The request that contains the resource whose image it to be loaded as well as other request options. The implementation should create a deep copy of the request so that it can't be changed underneath it later. The implementation may provide more request options that are available in a base class, so make sure to check the documentation on that.
+ @param completion Can be called synchronously. A block to be called when image loading is complete, providing the requested image or information about the status of the request. The info dictionary provides information about the status of the request. See the definitions of DFImageInfo*Key strings for possible keys and values.
+ @return An image task.
+ */
+- (nullable DFImageTask *)requestImageForRequest:(DFImageRequest *)request completion:(void (^__nullable)(UIImage *__nullable image, NSDictionary *info))completion DEPRECATED_ATTRIBUTE;
 
 @end
 
