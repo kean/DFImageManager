b8b708c8b2b82a87dec7cd7fc794e553aa921537
kean <grebenyuk.alexander@gmail.com>
2015-09-04 11:23:04 +0300

    Update demo project

--- Demo/DFImageManagerSample/Source/View Controllers/NSURLSession Demo View Controller/SDFBuiltinNetworkingDemoViewController.m
+++ Demo/DFImageManagerSample/Source/View Controllers/NSURLSession Demo View Controller/SDFBuiltinNetworkingDemoViewController.m
@@ -49,7 +49,7 @@ static NSString * const reuseIdentifier = @"Cell";
     
     DFURLImageFetcher *fetcher = [[DFURLImageFetcher alloc] initWithSessionConfiguration:configuration];
     id<DFImageManaging> manager = [[DFImageManager alloc] initWithConfiguration:[DFImageManagerConfiguration configurationWithFetcher:fetcher processor:[DFImageProcessor new] cache:[DFImageCache new]]];
-    [DFImageManager addSharedManager:manager];
+    [DFImageManager setSharedManager:manager];
 }
 
 - (void)viewDidLoad {
